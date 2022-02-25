4cb50c76b34795b3a6b3be560ab19bba4175a7d2
danifullstack <danifullstackengineer@gmail.com>
2022-02-25 04:03:51 +0100

    finished header

---
+++ amazonclone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvJl9hT1RrssI-2l4Jvn3ZauNhPasiPWw",
+  authDomain: "clone-ccdba.firebaseapp.com",
+  projectId: "clone-ccdba",
+  storageBucket: "clone-ccdba.appspot.com",
+  messagingSenderId: "786911348733",
+  appId: "1:786911348733:web:465616c86862d64672fb94",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
